a2056749dee32f1de9a916ac1bda41627266a5f3
Ralph Zühlsdorf <rh@Ralphs-MBP-2.fritz.box>
2020-04-19 22:54:12 +0200

    mix fix and README

--- mix.exs
+++ mix.exs
@@ -1,34 +1,44 @@
 defmodule Apa.MixProject do
-  use Mix.Project
-
   @moduledoc """
   Standard `MixProject`.
   """
+  use Mix.Project
 
   def project do
     [
       app: :apa,
-      version: "0.1.0",
+      version: "0.3.0",
       elixir: "~> 1.10",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "Apa",
+      source_url: "https://github.com/razuf/apa",
+      docs: [main: "readme", extras: ["README.md"]],
+      description: description(),
+      package: package()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:credo, "~> 1.3", only: [:dev, :test], runtime: false},
-      {:stream_data, "~> 0.1", only: :test}
+      {:stream_data, "~> 0.1", only: :test},
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp description() do
+    "Arbitrary precision arithmetic."
+  end
+
+  defp package() do
+    [
+      maintainers: ["Ralph Zühlsdorf"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/razuf/apa"}
     ]
   end
 end
